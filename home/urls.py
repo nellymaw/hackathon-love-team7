58a177164aa259a3277472f2c5656d2a91a4a491
Daniel Callaghan <dan_cal89@hotmail.com>
2022-02-25 11:37:45 +0000

    add pass reset

--- home/urls.py
+++ home/urls.py
@@ -3,10 +3,15 @@ from django.contrib.auth import views as auth_views
 from .views import landing, sign_up, logout_view
 
 urlpatterns = [
+
     path('', landing, name='landing-home'),
     path('sign-up/', sign_up, name='sign-up'),
     path('login/', auth_views.LoginView.as_view(template_name='home/login.html'), name='login'),
-    path('logout/', logout_view, name='logout')
+    path('logout/', logout_view, name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='home/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='home/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='home/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='home/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 
