a67eca54900ade4c1ba454a066ee255180ee2583
AlexNexton <cianoc13@gmail.com>
2022-02-25 22:20:29 +0000

    initail profiles app set up

---
+++ profiles/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+
+    """
+    A user Profile model for maintaining default
+    information and history
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    UserProfile.objects.create(user=instance)
+
+    """
+    Create or update user profiles
+    """
+    if created:
+        #Exisiting users: just save profile
+        instance.userprofile.save()
+ 
