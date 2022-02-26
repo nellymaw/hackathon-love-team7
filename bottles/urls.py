f7519ac56df9c721be784971e01a20d14f712bf5
Daniel Callaghan <84312595+xiaoniuniu89@users.noreply.github.com>
2022-02-26 11:59:53 +0000

    Bop n fresh (#59)
    * deployment commit
    
    * settings
    
    * bottles

--- bottles/urls.py
+++ bottles/urls.py
@@ -1,9 +1,10 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.LetterList.as_view(), name='letter_list'),
     path('<slug:slug>/', views.LetterDetail.as_view(), name='letter_detail'),
+    path('ocean', views.ocean, name='bottles-home'),
 ]
+
 
---
+++ static/bottles/js/ocean.js
@@ -0,0 +1,15 @@
+const bottles = document.querySelectorAll('.bottle')
+
+bottles.forEach((bottle) => {
+  bottle.addEventListener('click', (e) => {
+      let letter = e.target.nextElementSibling
+      if(letter.classList.contains('hidden')){
+          letter.classList.remove('hidden')
+          letter.classList.add('card')
+
+      }else {
+          letter.classList.add('hidden')
+          letter.classList.remove('card')
+      }
+  });
+});
