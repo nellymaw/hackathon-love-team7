354f321fd178fa7c0658117a5c3c3f78a576d71c
AlexNexton <cianoc13@gmail.com>
2022-02-26 03:43:13 +0000

    DB: added initial setup of  aullauth

--- message_in_a_bottle/settings.py
+++ message_in_a_bottle/settings.py
@@ -48,7 +48,14 @@ INSTALLED_APPS = [
     'home',
     'bottles',
     'profiles',
+    
+
+    'allauth',  # added for login
+    'allauth.account',
+    'allauth.socialaccount',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -63,19 +70,30 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'message_in_a_bottle.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [TEMPLATES_DIR],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.template.context_processors.media',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
         },
-    },
+    ]
+   
+AUTHENTICATION_BACKENDS = [
+    
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+    
 ]
 
 WSGI_APPLICATION = 'message_in_a_bottle.wsgi.application'
