c8ec54a2415c8bda76b814ba996e808c9519e04c
Christopher Carabine <85298460+ccarabine@users.noreply.github.com>
2022-02-26 16:51:55 +0000

    Add contact form, gmail settings (#61)

--- bottles/urls.py
+++ bottles/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.LetterList.as_view(), name='letter_list'),
     path('<slug:slug>/', views.LetterDetail.as_view(), name='letter_detail'),
     path('ocean', views.ocean, name='bottles-home'),
+    path('contact/<slug:slug>/', views.ContactView, name="contact"),
 ]
-
-
