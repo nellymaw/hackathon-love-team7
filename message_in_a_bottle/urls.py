cae062c10be70c51ae7a17f938ea235093f2a849
AlexNexton <cianoc13@gmail.com>
2022-02-26 20:46:12 +0000

    fix: take two merge conflicts

--- message_in_a_bottle/urls.py
+++ message_in_a_bottle/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    # path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('profile/', include('profiles.urls'))
 
@@ -29,3 +29,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
