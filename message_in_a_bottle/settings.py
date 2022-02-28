b442c549f6732c94f87457f956bc6314b294cc8f
Christopher Carabine <85298460+ccarabine@users.noreply.github.com>
2022-02-28 14:55:31 +0000

    Deploy (#105)
    * Add production allowed host
    
    * crispy forms

--- message_in_a_bottle/settings.py
+++ message_in_a_bottle/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'crispy_forms',
     'cloudinary',
     'home',
     'bottles',
