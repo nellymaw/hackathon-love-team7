908b9ffb8a3e873bbee4a9307ec6b483461fced4
Christopher Carabine <85298460+ccarabine@users.noreply.github.com>
2022-02-28 14:50:43 +0000

    Add production allowed host (#104)

--- message_in_a_bottle/settings.py
+++ message_in_a_bottle/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hackathon-love-team7.herokuapp.com', 'pipeline-love-team7-staging.herokuapp.com', 'localhost']
-
+ALLOWED_HOSTS = ['hackathon-love-team7.herokuapp.com', 'pipeline-love-team7.herokuapp.com', 'pipeline-love-team7-staging.herokuapp.com', 'localhost']
 
 # Application definition
 
